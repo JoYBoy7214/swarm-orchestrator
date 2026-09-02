df8d0c773f21f4b51d327915cd5bce3a856ded4d
Gowtham Balaji <gowthambalaji1472@gmail.com>
2026-09-02 22:28:24 +0530

    phase 3 done

--- orchestrator/cmd/api/main.go
+++ orchestrator/cmd/api/main.go
@@ -2,100 +2,76 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
-	sub "github.com/JoYBoy7214/swarm-orchestrator/cmd/sub"
-	pub "github.com/JoYBoy7214/swarm-orchestrator/internal/pub"
-	"github.com/google/uuid"
+	"github.com/JoYBoy7214/swarm-orchestrator/internal/orchestrator"
 	"github.com/nats-io/nats.go"
 )
 
-type Payload struct {
-	Url string `json:"url"`
-}
-type UserRequest struct {
-	TaskType string  `json:"task_type"`
-	Payload  Payload `json:"payload"`
-}
-type Event struct {
-	EventID   uuid.UUID   `json:"event_id"`
-	UserReq   UserRequest `json:"user_req"`
-	Status    string      `json:"status"`
-	CreatedAt time.Time   `json:"created_at"`
-}
-type Response struct {
-	EventID string `json:"task_id"`
-}
-
-type NServer struct {
-	Jstream *pub.JetStreamHandler
-}
-
-func (ns *NServer) TaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-	var req UserRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	//log.Println(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	e := UserReqToEvent(req)
-	b, err := json.Marshal(e)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	subject_string := "task." + req.TaskType
-	err = ns.Jstream.Publish(b, ctx, subject_string)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	res := Response{
-		EventID: e.EventID.String(),
-	}
-	w.WriteHeader(http.StatusAccepted)
-	err = json.NewEncoder(w).Encode(res)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-}
-func UserReqToEvent(req UserRequest) Event {
-	id := uuid.New()
-	t := time.Now()
-	return Event{
-		EventID:   id,
-		UserReq:   req,
-		Status:    "Pending",
-		CreatedAt: t,
-	}
-}
-
 func main() {
-	var err error
-	var nc *nats.Conn
-	var server NServer
-	server.Jstream, nc, err = pub.CreateStream()
-	defer nc.Close()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	orch, err := orchestrator.CreateOrchestrator(ctx, "postgres://postgres:postgres123@localhost:5432/postgres?sslmode=disable", nats.DefaultURL)
 	if err != nil {
-		log.Fatal("error in creating JetStream", err)
+		log.Fatal(err)
 		return
 	}
-	go sub.StatusListenerSub(server.Jstream.Jstream, "task.status.>")
-	http.HandleFunc("POST /api/v1/tasks", server.TaskHandler)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-	log.Println("Server is Started at 8080")
-	if http.ListenAndServe(":8080", nil) != nil {
-		log.Fatal("failed to start the server at 8080")
-		return
+	go func() {
+		defer wg.Done()
+		orch.StartOrchestrating(ctx)
+	}()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/v1/tasks", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		rctx, rcancel := context.WithTimeout(r.Context(), 5*time.Second)
+		defer rcancel()
+		workflow_id, err := orch.DbDriver.CreateWorkflow(rctx)
+		if err != nil {
+			log.Println("Error in creating workflow %w", err)
+			http.Error(w, "Error in creating workflow", http.StatusInternalServerError)
+			return
+		}
+
+		err = orch.RootNodeUpdater(rctx, workflow_id)
+		if err != nil {
+			log.Println("Error in updating workflow %w", err)
+			http.Error(w, "Error in creating workflow", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
 
+	go func() {
+		log.Println("Server started at :8080")
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("shutdowning grace fully")
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("HTTP shutdown error: %v", err)
+	}
+	wg.Wait()
+	stop() //this will not wait it will just trigger the signal
+	log.Println("Application stopped cleanly.")
 }
